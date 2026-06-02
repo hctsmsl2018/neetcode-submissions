d569ea5b5e6bbbfc92ef55450b036c259d4abb81
Derek Xu <44418630+hctsmsl2018@users.noreply.github.com>
2026-06-02 02:20:42 -0700

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [0] * (len(t) + 1)
+        dp[0] = 1
+        for c in s:
+            # iterate backwards to use previous row values
+            for j in range(len(t), 0, -1):
+                if c == t[j - 1]:
+                    dp[j] += dp[j - 1]
+        return dp[len(t)]
